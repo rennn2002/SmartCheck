76a1135301ddb628a98dafbd9fc49ab8dac36420
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-09-28 06:06:23 +0900

    Initial Commit

---
+++ Health Check/Health_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_CheckApp.swift
+//  Health Check
+//
+//  Created by Nomura Rentaro on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct Health_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
