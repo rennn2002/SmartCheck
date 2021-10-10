7fffe94f452a186d71feeab93122926447248b68
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-10-11 03:12:23 +0900

    changed re sent button

--- Health Check/CheckFormView.swift
+++ Health Check/CheckFormView.swift
@@ -11,6 +11,11 @@ struct CheckFormView: View {
     @State var bodyTemp: Float = 0.0
     @State var symptom: Bool = false
     @State var postTime: String = "not provided"
+    @State var currentTime: String = "not provided"
+    @State var postDay: Date = Date()
+    
+    @State var currentDay: String = ""
+    @State var postDate: String = ""
     
     var fireauth: FireAuth = FireAuth()
     @ObservedObject var firestore: FireStore = FireStore()
@@ -19,7 +24,7 @@ struct CheckFormView: View {
         VStack {
             Image("check")
                 .resizable()
-                .frame(width:150, height:150)
+                .frame(width:UIScreen.main.bounds.width*0.35, height: UIScreen.main.bounds.width*0.35)
             
             Text("送信されました")
                 .foregroundColor(.green)
@@ -28,6 +33,7 @@ struct CheckFormView: View {
             
             Spacer(minLength: 30)
                 .fixedSize()
+            
             HStack {
                 Text("体温：")
                     .fontWeight(.bold)
@@ -36,6 +42,7 @@ struct CheckFormView: View {
                     .fontWeight(.bold)
                     .font(.title3)
             }.padding(2.5)
+            
             HStack {
                 Text("自覚症状：")
                     .fontWeight(.bold)
@@ -44,6 +51,7 @@ struct CheckFormView: View {
                     .fontWeight(.bold)
                     .font(.title3)
             }.padding(2.5)
+            
             HStack {
                 Text("提出時間：")
                     .fontWeight(.bold)
@@ -54,36 +62,61 @@ struct CheckFormView: View {
             }.padding(2.5)
             
             Spacer(minLength: 0)
+            
             Button(action: {
                 UserDefaults.standard.set(false, forKey: "isFormPosted")
                 NotificationCenter.default.post(name: NSNotification.Name("isFormPosted"), object: nil)
             }) {
                 Text("再提出")
-                    .foregroundColor(.white)
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 50)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.top, 5)
+                    .font(.body)
+                    .foregroundColor(.blue)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             }.padding(25)
         }.onAppear {
             fireauth.getData()
-            firestore.getForm(uid: fireauth.uid)
-            
-            self.bodyTemp = firestore.tempdata.bodytemp
-            self.symptom = firestore.tempdata.symptom
-            
-            //convert timestamp to date
-            let date: Date = firestore.tempdata.posttime.dateValue()
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.locale = Locale(identifier: "ja_JP")
-            dateFormatter.dateStyle = .medium
-            dateFormatter.dateFormat = "yyyy/MM/d HH:mm"
-            
-            self.postTime = dateFormatter.string(from: date)
+            firestore.getForm(uid: fireauth.uid) { result in
+                if result {
+                    self.bodyTemp = firestore.tempdata.bodytemp
+                    self.symptom = firestore.tempdata.symptom
+                    
+                    //convert timestamp to date
+                    let date: Date = firestore.tempdata.posttime.dateValue()
+                    
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.locale = Locale(identifier: "ja_JP")
+                    dateFormatter.dateStyle = .medium
+                    dateFormatter.dateFormat = "yyyy/MM/d HH:m"
+                    
+                    self.postTime = dateFormatter.string(from: date)
+                    
+                    dateFormatter.locale = Locale(identifier: "ja_JP")
+                    dateFormatter.dateStyle = .medium
+                    dateFormatter.dateFormat = "d"
+                    self.postDate = dateFormatter.string(from: date)
+                    
+                    //obtain current time
+                    let dt = Date()
+                    dateFormatter.locale = Locale(identifier: "ja_JP")
+                    dateFormatter.dateStyle = .medium
+                    dateFormatter.dateFormat = "yyyy/MM/d HH:m"
+                    
+                    self.currentTime = dateFormatter.string(from: dt)
+                    dateFormatter.locale = Locale(identifier: "ja_JP")
+                    dateFormatter.dateStyle = .medium
+                    dateFormatter.dateFormat = "d"
+                    self.currentDay = dateFormatter.string(from: dt)
+                    
+                    
+                    //compare current time and post time
+                    if self.postDate != self.currentDay {
+                        UserDefaults.standard.set(false, forKey: "isFormPosted")
+                        NotificationCenter.default.post(name: NSNotification.Name("isFormPosted"), object: nil)
+                    } else {
+                        UserDefaults.standard.set(true, forKey: "isFormPosted")
+                        NotificationCenter.default.post(name: NSNotification.Name("isFormPosted"), object: nil)
+                    }
+                }
+            }
         }
     }
 }
