4a87504ee0c0f1bcbcb1c66679f326d86e4e1fa6
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-10-11 03:14:13 +0900

    added waiting view and changed design

--- Health Check/LaunchView.swift
+++ Health Check/LaunchView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import FirebaseAuth
 
 struct LaunchView: View {
     @State var showLogin = false
@@ -13,48 +15,42 @@ struct LaunchView: View {
     @State var isLoggedin = UserDefaults.standard.value(forKey: "isLoggedin") as? Bool ?? false
     @State var isSignedup = UserDefaults.standard.value(forKey: "isSignedup") as? Bool ?? false
     @State var isGuidanceShow = UserDefaults.standard.value(forKey: "isGuidanceShow") as? Bool ?? false
+    @State var isWaitingShow = UserDefaults.standard.value(forKey: "isWaitingShow") as? Bool ?? false
     
     //var googleauth: GoogleAuth = GoogleAuth()
+    let backGroundColor = Color(red: 96/255, green: 123/255, blue: 184/255)
     
     var body: some View {
         NavigationView {
+            ZStack {
+            backGroundColor.edgesIgnoringSafeArea(.all)
             VStack {
                 if self.isLoggedin {
                     HomeView()
+                } else if self.isWaitingShow {
+                    WaitingView()
                 } else if self.isSignedup {
                     ProfileInitView()
                 } else if self.isGuidanceShow {
                     GuidanceView()
                 } else {
+                    Spacer(minLength:0)
                     Text("健康観察入力アプリ")
+                        .foregroundColor(.white)
                         .font(.largeTitle)
                         .fontWeight(.bold)
+                        .padding()
 //                    Text("聖マリアンナ医科大学")
 //                        .font(.title3)
 //                        .foregroundColor(.gray)
 //                        .fontWeight(.bold)
+                    Spacer(minLength:0)
                     
-                    Spacer(minLength: 30)
-                        .fixedSize()
                     Image("health-check")
                         .resizable()
-                        .frame(width:200, height:200)
+                        .frame(width:UIScreen.main.bounds.width*0.7, height:UIScreen.main.bounds.width*0.7)
                     
-                    Spacer(minLength:80)
-                        .fixedSize()
-                    
-                    NavigationLink(destination: LoginView(showLogin: self.$showLogin), isActive: self.$showLogin)  {
-                        Text("ログイン")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding(.vertical)
-                            .frame(width:UIScreen.main.bounds.width - 50)
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                            .padding(.top, 25)
-                        
-                    }
+                    Spacer(minLength:0)
                     
                     NavigationLink(destination: SignupView(showSignup: self.$showSignup), isActive: self.$showSignup)  {
                         Text("新規登録")
@@ -63,17 +59,56 @@ struct LaunchView: View {
                             .fontWeight(.bold)
                             .padding(.vertical)
                             .frame(width:UIScreen.main.bounds.width - 50)
-                            .background(Color.green)
-                            .cornerRadius(10)
+                            .background(Color(red: 119/255, green: 140/255, blue: 236/255))
+                            .cornerRadius(50)
                             .padding(.top, 25)
                     }
-                   
-   
+                    
+                    NavigationLink(destination: LoginView(showLogin: self.$showLogin), isActive: self.$showLogin)  {
+                        Text("ログイン")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.vertical)
+                            .frame(width:UIScreen.main.bounds.width - 50)
+                            .background(Color(red: 62/255, green: 74/255, blue: 141/255))
+                            .cornerRadius(50)
+                            .padding(.top, 25)
+                        
+                    }
+                                
+                    Button(action:{
+                        GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.first?.rootViewController
+                        GIDSignIn.sharedInstance()?.signIn()
+                    }) {
+                        HStack {
+                            Image("google-color")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.white)
+                            
+                            Spacer(minLength: 20)
+                                .fixedSize()
+                            Text("Google Sign-In")
+                                .foregroundColor(.gray)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .padding(.vertical)
+                        }.frame(width:UIScreen.main.bounds.width - 80, height: 50)
+                        .clipShape(Capsule())
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .padding(.top, 25)
+                        
+                    }
+                    Spacer(minLength:30)
                     //GoogleSignInButtonViewController()
                 }
             }
+            
             .navigationBarHidden(true)
             .navigationBarBackButtonHidden(true)
+            }
         }.navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
         .onAppear {
@@ -85,6 +120,9 @@ struct LaunchView: View {
             }
             NotificationCenter.default.addObserver(forName: NSNotification.Name("isGuidanceShow"), object: nil, queue: .main) { (_) in
                 self.isGuidanceShow = UserDefaults.standard.value(forKey: "isGuidanceShow") as? Bool ?? false
+            }
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("isWaitingShow"), object: nil, queue: .main) { (_) in
+                self.isWaitingShow = UserDefaults.standard.value(forKey: "isWaitingShow") as? Bool ?? false
             }
         }
     }
