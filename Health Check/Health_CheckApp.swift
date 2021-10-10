be50c6dceaafcd9003430ed614d8dfa6d31009e3
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-10-11 03:10:23 +0900

    Add GID

--- Health Check/Health_CheckApp.swift
+++ Health Check/Health_CheckApp.swift
@@ -36,6 +36,7 @@
 
 import SwiftUI
 import Firebase
+import GoogleSignIn
 
 @main
 struct MedsalonApp: App {
@@ -47,23 +48,67 @@ struct MedsalonApp: App {
     }
 }
 
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate ,GIDSignInDelegate {
     var notification: Notification = Notification()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        UNUserNotificationCenter.current()
-//          .requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
-//            print("Permission granted: \(granted)")
-//          }
-//        UNUserNotificationCenter.current().delegate = self
-//        let appDomain = Bundle.main.bundleIdentifier
-//        UserDefaults.standard.removePersistentDomain(forName: appDomain!)
+        //        UNUserNotificationCenter.current()
+        //          .requestAuthorization(options: [.alert, .sound, .badge]) { (granted, _) in
+        //            print("Permission granted: \(granted)")
+        //          }
+        //        UNUserNotificationCenter.current().delegate = self
+        //        let appDomain = Bundle.main.bundleIdentifier
+        //       UserDefaults.standard.removePersistentDomain(forName: appDomain!)
         NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: nil) { _ in
             print("entered")
             self.notification.makeNotificationOnSchedule()
         }
         FirebaseApp.configure()
-
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        UserDefaults.standard.set(true, forKey: "isWaitingShow")
+        NotificationCenter.default.post(name: NSNotification.Name("isWaitingShow"), object: nil)
+        UserDefaults.standard.set(false, forKey: "isGuidanceShow")
+        NotificationCenter.default.post(name: NSNotification.Name("isGuidanceShow"), object: nil)
+        if error != nil{
+            
+            print(error.localizedDescription)
+            return
+        }
+        let credential = GoogleAuthProvider.credential(withIDToken: user.authentication.idToken, accessToken: user.authentication.accessToken)
+        
+        Auth.auth().signIn(with: credential) { (res, err) in
+            
+            if err != nil{
+                
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            if let user = Auth.auth().currentUser {
+                let db = Firestore.firestore()
+                let ref = db.collection("users").document(user.uid)
+                ref.getDocument { (document, error) in
+                    if let document = document, document.exists {
+                        print("exist")
+                        UserDefaults.standard.set(false, forKey: "isWaitingShow")
+                        NotificationCenter.default.post(name: NSNotification.Name("isWaitingShow"), object: nil)
+                        UserDefaults.standard.set(true, forKey: "isLoggedin")
+                        NotificationCenter.default.post(name: NSNotification.Name("isLoggedin"), object: nil)
+                    } else {
+                        print("new")
+                        UserDefaults.standard.set(false, forKey: "isWaitingShow")
+                        NotificationCenter.default.post(name: NSNotification.Name("isWaitingShow"), object: nil)
+                        UserDefaults.standard.set(true, forKey: "isSignedup")
+                        NotificationCenter.default.post(name: NSNotification.Name("isSignedup"), object: nil)
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
